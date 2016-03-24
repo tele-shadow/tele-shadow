a12c284803e5c47edf035c4ee2c1fa3cc92b7e02
tele-shadow <misagh102311@live.com>
2016-03-24 15:37:47 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {71873268},--Sudo users
+    sudo_users = {71873268,96700621},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
